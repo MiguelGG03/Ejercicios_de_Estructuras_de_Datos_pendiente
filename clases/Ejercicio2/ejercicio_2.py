99405d7a53e2a256e7550b458064c8f7173ac230
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-17 16:57:37 +0200

    ejercicio 2 acabado al 100%

--- clases/Ejercicio2/ejercicio_2.py
+++ clases/Ejercicio2/ejercicio_2.py
@@ -1,20 +1,34 @@
 class FilosofiaMVC:
+
     def __init__(self):
-        self.mensaje=str(input("Introduzca el mensaje: "))
+        self.mensaje1=str(input("Introduzca el mensaje: "))
+        self.mensaje2=str(input("Introduzca el mensaje: "))
     
+    
+
     def vista(self):
         troceado=self.troceador()
-        f=open('clases.Ejercicio2.salida.txt', 'w','cp1252')
-        print(f)
+        f=open('salida.txt', 'w')
         f.write(troceado)
-            
+        f.close
+        self.f=f
 
-    
+
+
     def troceador(self):
+        t1=[]
+        t2=[]
         troceado=[]
-        for i in self.mensaje:
-            troceado.append(str(i.upper()))
-        tyu="".join(troceado)
+        for i in self.mensaje1:
+            t1.append(str(i.upper()))
+        for i in self.mensaje2:
+            t2.append(str(i.upper()))
+        tyu1="".join(t1)
+        tyu2="".join(t2)
+        troceado.append(tyu1)
+        troceado.append(tyu2)
+        tyu="\n".join(troceado)
+        print(tyu)
         return tyu
 
 def main():
