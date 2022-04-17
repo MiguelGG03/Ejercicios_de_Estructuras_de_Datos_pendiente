cd3e7f804b54fbe8b4860269a8026718f1a86fbb
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-17 13:49:00 +0200

    Instruccion en vez de Instruction

--- clases/Ejercicio1/ejercicio_1.py
+++ clases/Ejercicio1/ejercicio_1.py
@@ -2,7 +2,7 @@ class Bloque:
     def __init__(self): 
         self.instrucciones = []
  
-    def agregarInstruction(self, instruccion): 
+    def agregarInstruccion(self, instruccion): 
         self.instrucciones.append(instruccion)
 
 
@@ -32,7 +32,7 @@ def main():
     mostrar_ko = Mostrar('"KO"') 
     alternativa = Si("2 + 2 == 4", mostrar_ok, mostrar_ko) 
     bloque_alternativa = Bloque() 
-    bloque_alternativa.agregarInstruction(alternativa) 
+    bloque_alternativa.agregarInstruccion(alternativa) 
     bucle = MientrasQue(True, bloque_alternativa) 
     print(bucle)
 
